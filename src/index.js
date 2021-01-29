57e8c08673f7903f1d11f8dad2672faced2269b1
MaksimTsvik <mkdemix@tut.by>
2021-01-29 15:04:48 +0300

    Added max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 
 exports.min = function min(array) {
-  array.sort((a, b) => a - b); // We sort this array ascending, so 1st arr element is equal to min
-  return array[0]; //Return 1st arr element
+  // We sort this array ascending, so 1st arr element is equal to min
+  array.sort((a, b) => a - b);
+  //Return 1st arr element
+  return array[0];
 }
 
 exports.max = function max(array) {
-  return 0;
+  // We sort this array decending, so 1st arr element is equal to max
+  array.sort((a, b) => b - a);
+  //Return 1st arr element
+  return array[0];
 }
 
 exports.avg = function avg(array) {
-  return 0;
+  //Calculate average by summarazing all array elements and than divide
+  let result = array.reduce((sumOfElements, nextElement) => sumOfElements + nextElement, 0) / array.length;
+  return result;
 }
