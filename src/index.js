cae37af84eb70217307b2b50190fdf839ee1f922
MaksimTsvik <mkdemix@tut.by>
2021-01-29 15:52:23 +0300

    Added array inspection and tested

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 
 exports.min = function min(array) {
+  //Inspect if the array is undefined and if it is epmty
+  if (array === undefined || !array.length) {
+    return 0;
+  }
   // We sort this array ascending, so 1st arr element is equal to min
   array.sort((a, b) => a - b);
   //Return 1st arr element
@@ -7,6 +11,10 @@ exports.min = function min(array) {
 }
 
 exports.max = function max(array) {
+  //Inspect if the array is undefined and if it is epmty
+  if (array === undefined || !array.length) {
+    return 0;
+  }
   // We sort this array decending, so 1st arr element is equal to max
   array.sort((a, b) => b - a);
   //Return 1st arr element
@@ -14,6 +22,10 @@ exports.max = function max(array) {
 }
 
 exports.avg = function avg(array) {
+  //Inspect if the array is undefined and if it is epmty
+  if (array === undefined || !array.length) {
+    return 0;
+  }
   //Calculate average by summarazing all array elements and than divide
   let result = array.reduce((sumOfElements, nextElement) => sumOfElements + nextElement, 0) / array.length;
   return result;
