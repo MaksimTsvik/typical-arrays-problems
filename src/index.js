148ca57a1b3fe2cbb38ded5af18824183be04bc7
MaksimTsvik <mkdemix@tut.by>
2021-01-29 14:47:19 +0300

    Initial commit - min function updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+  array.sort((a, b) => a - b); // We sort this array ascending, so 1st arr element is equal to min
+  return array[0]; //Return 1st arr element
+}
+
+exports.max = function max(array) {
   return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
   return 0;
 }
